2e314a63dcb33d15a955ec0f1e5a8811f08fd7d9
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-14 09:00:45 +0800

    refactor(archive): bring some new features

--- archive.sh
+++ archive.sh
@@ -1,16 +1,40 @@
-git-archive-all -h 2&>/dev/null >/dev/null
+gethash() {
+	cd $1
+	val=$(git rev-parse --short HEAD)
+	cd ..
+	echo $val
+}
 
-if [ ! "$?" = "0" ]
+check_archive_all() {
+	git-archive-all -h 2&>/dev/null >/dev/null
+
+	if [ ! "$?" = "0" ]
+		then
+		echo -e "\x1b[1;38;5;196mYou should install git-archive-all first.\x1b[0m"
+		echo ""
+		echo "For macOS systems with Homebrew, install it with:"
+		echo "    brew install git-archive-all"
+		echo "Otherwise, install it from pip:"
+		echo "    pip install git-archive-all"
+		exit 1
+	fi
+}
+
+update_subm() {
+	bash update.sh >/dev/null	
+}
+
+
+if [ "$1" = "--update" ]
 then
-	echo -e "\x1b[1;38;5;196mYou should install git-archive-all first.\x1b[0m"
-	echo ""
-	echo "For macOS systems with Homebrew, install it with:"
-	echo "    brew install git-archive-all"
-	echo "Otherwise, install it from pip:"
-	echo "    pip install git-archive-all"
-	exit 1
+	update_subm
 fi
 
-FILENAME="archive-$(date +"%Y-%m-%d-%H-%M-%S").zip"
+FE_HASH="$(gethash frontend)"
+BE_HASH="$(gethash backend)"
+FILENAME="archive-$FE_HASH-$BE_HASH.zip"
+
 git-archive-all --force-submodules "$FILENAME"
+
+# Output the FILENAME for the next task.
 echo $FILENAME
